e0c0df7820bdedf75b9fe938c2fba9c83c814c36
Dinesh Poudel <dineshp4297501@gmail.com>
2026-05-05 20:07:49 +0545

    modified Program.cs

--- ObjectOriented/Program.cs
+++ ObjectOriented/Program.cs
@@ -6,8 +6,17 @@ namespace ObjectOriented
     {
         static void Main(string[] args)
         {
-            ConstructorExample example = new ConstructorExample();
-            example.run();
+            // Using Constructor 1
+            Student s1 = new Student("Sauman", "A");
+            Console.WriteLine("Student 1:");
+            s1.DisplayInfo();
+
+            Console.WriteLine();
+
+            // Using Constructor 2
+            Student s2 = new Student("Bobby");
+            Console.WriteLine("Student 2:");
+            s2.DisplayInfo();
         }
     }
 }
